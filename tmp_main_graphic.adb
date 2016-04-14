a19c9f12354e47236336b45275d8b2298a1fde39
Jonas Olaussen <jonol044@parlomba2.ida.liu.se>
2016-04-14 22:28:46 +0200

    Tog bort filer

--- tmp_main_graphic.adb
+++
@@ -1,9 +0,0 @@
-with Chess_Game_Graphic;            use Chess_Game_Graphic;
-with Chess_Game_Handling;           use Chess_Game_Handling;
-procedure Tmp_Main_Graphic is
-   X,Y : Integer; 
-begin
-   Draw_Complete_Game_Board;
-   Choose_Active_Chessman(X,Y); 
-end Tmp_Main_Graphic;
-
